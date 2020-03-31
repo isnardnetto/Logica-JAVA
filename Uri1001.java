25758f58b862fddac96cb5c701482d1e03305325
isnard <engisnard@gmail.com>
2020-03-31 20:46:01 -0300

    Adicionei os codigos com a extensão em java

--- Uri1001.java
+++ Uri1001.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Uri1001 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

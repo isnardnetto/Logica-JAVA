5328cfecc11b7b8edbddb5ed7ed11a6358e7416b
isnard <engisnard@gmail.com>
2020-04-04 20:13:02 -0300

    ex com while feito

---
+++ Uri1114.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int senha = sc.nextInt();
+
+        while (senha != 2002){
+            System.out.println("senha invaida");
+            senha = sc.nextInt();
+        }
+
+        System.out.println("acesso permitido");
+    }
+}

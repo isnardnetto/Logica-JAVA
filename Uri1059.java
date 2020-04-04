d7cb633ed84bc899bb179b5e1ec4487ccae19d85
isnard <engisnard@gmail.com>
2020-04-03 23:13:11 -0300

    Ex Uri1059 feito

---
+++ Uri1059.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+      int i ;
+
+      for(i=0; i<=100 ; i++){
+          if(i%2 == 0){
+              System.out.println(i);
+          }
+      }
+
+
+    }
+}
